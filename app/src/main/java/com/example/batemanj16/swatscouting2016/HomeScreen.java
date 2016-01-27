f15eab2ab93d86cce10602100be70efbbe9350dc
Jason Bateman <jasonbateman98@gmail.com>
2016-01-27 15:22:38 -0600

    Getting Basic Time functions set up... Thinking about putting them in their own class if there are too many of them

--- app/src/main/java/com/example/batemanj16/swatscouting2016/HomeScreen.java
+++ app/src/main/java/com/example/batemanj16/swatscouting2016/HomeScreen.java
@@ -1,7 +1,13 @@
 package com.example.batemanj16.swatscouting2016;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,5 +15,48 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        //How to get a button to more reliably do complex functions
+        final Button btnTest = (Button) findViewById(R.id.btnTest);
+        final EditText txtTime = (EditText) findViewById(R.id.txtField);
+        final EditText txtTimeElapsed = (EditText) findViewById(R.id.txtTimeElapsed);
+
+        final String startTime = "030702";
+
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtTime.setText(getCurrentTimeStamp());
+                txtTimeElapsed.setText(getTimeElapsed(startTime));
+
+
+
+            }
+        });
+
+
     }
+    //Getting TimeStamp... Will prove to be important.
+
+    public static String getCurrentTimeStamp(){
+        SimpleDateFormat s = new SimpleDateFormat("hhmmss");
+        String format = s.format(new Date());
+        return format;
+
+    }
+
+    public static String getTimeElapsed(String startTime){
+
+        SimpleDateFormat newTimeFormat = new SimpleDateFormat("hhmmss");
+        String newTime = newTimeFormat.format(new Date());
+        int timeElapsed = Integer.parseInt(newTime) - Integer.parseInt(startTime);
+        return Integer.toString(timeElapsed);
+
+
+
+    }
+
+
+
 }
