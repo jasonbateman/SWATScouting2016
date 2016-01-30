e910396be050af5fb369e47d2ac78c7468962c4f
robotclayton <clayton.liberty17@gmail.com>
2016-01-29 18:36:56 -0600

    My first push

---
+++ app/src/main/java/com/example/batemanj16/swatscouting2016/scoutScreen.java
@@ -0,0 +1,13 @@
+package com.example.batemanj16.swatscouting2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class scoutScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scout_screen);
+    }
+}
