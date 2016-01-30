ca3f233357c57c78371729959183804d488fa9f7
robotclayton <clayton.liberty17@gmail.com>
2016-01-29 20:06:18 -0600

    More Scouting, Drop down list

--- app/src/main/java/com/example/batemanj16/swatscouting2016/scoutScreen.java
+++ app/src/main/java/com/example/batemanj16/swatscouting2016/scoutScreen.java
@@ -2,6 +2,8 @@ package com.example.batemanj16.swatscouting2016;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class scoutScreen extends AppCompatActivity {
 
@@ -9,6 +11,11 @@ public class scoutScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scout_screen);
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner);
+                String[] items =new String[]{"1", "2", "3"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item);
+        dropdown.setAdapter(adapter);
+                ;
     }
 }
 
