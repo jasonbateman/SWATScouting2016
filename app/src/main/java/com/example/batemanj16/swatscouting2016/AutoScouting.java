41c0456bffe229d6bf5ee894dcc272030d5c996b
Jason Bateman <jasonbateman98@gmail.com>
2016-02-03 16:05:11 -0600

    fu again clytn

---
+++ app/src/main/java/com/example/batemanj16/swatscouting2016/AutoScouting.java
@@ -0,0 +1,13 @@
+package com.example.batemanj16.swatscouting2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AutoScouting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auto_scouting);
+    }
+}
