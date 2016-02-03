bd38941b49c6fc89ebdb78f8205967576cdd89c5
robotclayton <clayton.liberty17@gmail.com>
2016-02-03 16:09:10 -0600

    Scouting goese to Auto

--- app/src/main/java/com/example/batemanj16/swatscouting2016/scoutScreen.java
+++ app/src/main/java/com/example/batemanj16/swatscouting2016/scoutScreen.java
@@ -3,6 +3,7 @@ package com.example.batemanj16.swatscouting2016;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class scoutScreen extends AppCompatActivity {
 
@@ -12,6 +13,12 @@ public class scoutScreen extends AppCompatActivity {
         setContentView(R.layout.activity_scout_screen);
     }
     Intent intent = getIntent();
+
+    public void goAuto (View view)
+    {
+        Intent intent = new Intent(this, AutoScouting.class);
+    startActivity(intent);
+    }
 
 
 }
