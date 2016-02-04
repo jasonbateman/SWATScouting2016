8013d9a9e6d8ea875e6460591bfac7c50697826d
Jason Bateman <jasonbateman98@gmail.com>
2016-02-03 18:20:44 -0600

    less ok clytn

--- app/src/main/java/com/example/batemanj16/swatscouting2016/scoutScreen.java
+++ app/src/main/java/com/example/batemanj16/swatscouting2016/scoutScreen.java
@@ -4,19 +4,21 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 
 public class scoutScreen extends AppCompatActivity {
-       boolean portcullis = false;
-    boolean cheval = false;
-    boolean moat = false;
-    boolean ramparts = false;
-    boolean draw = false;
-    boolean sally = false;
-    boolean rock = false;
-    boolean rough = false;
+    Boolean portcullis = false;
+    Boolean cheval = false;
+    Boolean moat = false;
+    Boolean ramparts = false;
+    Boolean draw = false;
+    Boolean sally = false;
+    Boolean rock = false;
+    Boolean rough = false;
+    Boolean logBool = false;
 
     boolean toAutoScreen = false;
 
@@ -37,14 +39,22 @@ public class scoutScreen extends AppCompatActivity {
         Runnable setUpListener = new Runnable(){
             @Override
             public void run() {
+                Log.d("RUNNABLE RUNNING", "The runnable is running");
                 //checking to see if each defense is represented
                 portcullis = ((spn_df1.getSelectedItem().toString()=="Portcullis")||(spn_df2.getSelectedItem().toString()=="Portcullis")||(spn_df3.getSelectedItem().toString()=="Portcullis")||(spn_df4.getSelectedItem().toString()=="Portcullis"));
-                cheval = ((spn_df1.getSelectedItem().toString()=="Cheval de Frise")||(spn_df2.getSelectedItem().toString()=="Cheval de Frise")||(spn_df3.getSelectedItem().toString()=="Cheval de Frise")||(spn_df4.getSelectedItem().toString()=="Cheval de Frise"));
+
+                cheval = ((spn_df1.getSelectedItem().toString()==getString(R.string.cheval))||(spn_df2.getSelectedItem().toString()==getString(R.string.cheval))||(spn_df3.getSelectedItem().toString()==getString(R.string.cheval))||(spn_df4.getSelectedItem().toString()==getString(R.string.cheval)));
+
                 moat = ((spn_df1.getSelectedItem().toString()=="Moat")||(spn_df2.getSelectedItem().toString()=="Moat")||(spn_df3.getSelectedItem().toString()=="Moat")||(spn_df4.getSelectedItem().toString()=="Moat"));
+
                 ramparts = ((spn_df1.getSelectedItem().toString()=="Ramparts")||(spn_df2.getSelectedItem().toString()=="Ramparts")||(spn_df3.getSelectedItem().toString()=="Ramparts")||(spn_df4.getSelectedItem().toString()=="Ramparts"));
+
                 draw = ((spn_df1.getSelectedItem().toString()=="Drawbridge")||(spn_df2.getSelectedItem().toString()=="Drawbridge")||(spn_df3.getSelectedItem().toString()=="Drawbridge")||(spn_df4.getSelectedItem().toString()=="Drawbridge"));
+
                 sally = ((spn_df1.getSelectedItem().toString()=="Sally Port")||(spn_df2.getSelectedItem().toString()=="Sally Port")||(spn_df3.getSelectedItem().toString()=="Sally Port")||(spn_df4.getSelectedItem().toString()=="Sally Port"));
+
                 rock = ((spn_df1.getSelectedItem().toString()=="Rock Wall")||(spn_df2.getSelectedItem().toString()=="Rock Wall")||(spn_df3.getSelectedItem().toString()=="Rock Wall")||(spn_df4.getSelectedItem().toString()=="Rock Wall"));
+
                 rough = ((spn_df1.getSelectedItem().toString()=="Rough Terrain")||(spn_df2.getSelectedItem().toString()=="Rough Terrain")||(spn_df3.getSelectedItem().toString()=="Rough Terrain")||(spn_df4.getSelectedItem().toString()=="Rough Terrain"));
 
 
@@ -57,7 +67,6 @@ public class scoutScreen extends AppCompatActivity {
                 if (((portcullis||cheval)&&(moat||ramparts)&&(draw||sally)&&(rough||rock))&&(toAutoScreen))
                 {
                     toAutoScreen = false;
-
                     Intent intent = new Intent(getApplicationContext(), AutoScouting.class);
                     startActivity(intent);
                 }
